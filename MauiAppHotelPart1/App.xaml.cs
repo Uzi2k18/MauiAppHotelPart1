fe05a189498dafd9d4cf93caa59b3075850c1607
Giovanni Batelo <147433055+Uzi2k18@users.noreply.github.com>
2024-11-18 02:14:52 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotelPart1/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿
+namespace MauiAppHotelPart1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Width = 600;
+
+            return window;                                               
+        }
+    }
+}
---
+++ MauiAppHotelPart1/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppHotelPart1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
